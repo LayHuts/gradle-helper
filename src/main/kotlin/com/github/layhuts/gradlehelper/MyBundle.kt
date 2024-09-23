38cfdb3f20548ae1787e58bd0258ad16be24425f
GitHub Action <action@github.com>
2024-09-23 09:39:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/layhuts/gradlehelper/MyBundle.kt
+++ src/main/kotlin/com/github/layhuts/gradlehelper/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.layhuts.gradlehelper
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/layhuts/gradlehelper/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/layhuts/gradlehelper/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.layhuts.gradlehelper.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
